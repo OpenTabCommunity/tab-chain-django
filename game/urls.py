a306d2eeb4fd9ad3eb4dc81f33f04311f4d88b58
Nima <nimars1394sh@gmail.com>
2025-11-07 14:35:10 -0600

    fix: urls  fixed

--- game/urls.py
+++ game/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PlayView, SessionDetailView, EndSessionView
+from .views import PlayView, SessionDetailView, EndSessionView, LeaderboardTopView
 
 urlpatterns = [
     path('play', PlayView.as_view()),
     path('session/<uuid:pk>', SessionDetailView.as_view()),
     path('session/<uuid:pk>/end', EndSessionView.as_view()),
+    path('leaderboard/top', LeaderboardTopView.as_view()),
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import SignupView, LoginView
+from .views import SignupView, LoginView, UserMeView, UserHistoryView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('signup', SignupView.as_view()),
     path('login', LoginView.as_view()),
     path('refresh', TokenRefreshView.as_view()),
+    path('me', UserMeView.as_view()),
+    path('<int:id>/history', UserHistoryView.as_view()),
 ]
