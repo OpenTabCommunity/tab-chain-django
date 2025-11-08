5dc8523ba67c3d491445755385ff4543421990c9
Nima <nimars1394sh@gmail.com>
2025-11-08 09:10:59 -0600

    fix: tests updated

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class AuthTests(APITestCase):
+    def setUp(self):
+        self.signup_url = '/api/auth/signup'
+        self.login_url = '/api/auth/login'
+
+    def test_signup_and_login(self):
+        res = self.client.post(self.signup_url, {'username': 'alice', 'password': 's3cret'})
+        self.assertEqual(res.status_code, 201)
+        res = self.client.post(self.login_url, {'username': 'alice', 'password': 's3cret'})
+        self.assertEqual(res.status_code, 200)
+        self.assertIn('access_token', res.data)
