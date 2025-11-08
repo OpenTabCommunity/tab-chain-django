d6b52ce49bb123a77ee073db8edf6bcd7b7e38bb
Nima <nimars1394sh@gmail.com>
2025-11-08 11:45:53 -0600

    add: users admin added

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from users.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ("id", "username", "email", "is_active", "is_staff", "date_joined")
+    list_filter = ("is_active", "is_staff", "is_superuser")
+    search_fields = ("username", "email")
+    ordering = ("-date_joined",)
+    readonly_fields = ("last_login", "date_joined")
+
+    fieldsets = (
+        ("Account Info", {
+            "fields": ("username", "email", "password")
+        }),
+        ("Permissions", {
+            "fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")
+        }),
+        ("Important Dates", {
+            "fields": ("last_login", "date_joined")
+        }),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "email", "password1", "password2"),
+        }),
+    )
